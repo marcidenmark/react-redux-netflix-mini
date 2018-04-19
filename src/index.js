363d6e2b4a9865d9eb53dba5e57cce354469db28
marci papineau <marcidenmark@hotmail.com>
2018-04-19 17:00:51 +0200

    added react-router and Provider in index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 // all above code came with the create react app
-const {Provider} = require ('react-redux');
+import { Provider } from 'react-redux';
+import { createStore }from 'react-redux';
+//const reducers = require ('/modules');
+//const routes = require ('/routes');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
