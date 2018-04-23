d4e13d2db48820e0ae29d4a12ce159f887b32bf3
marci papineau <marcidenmark@hotmail.com>
2018-04-23 10:44:50 +0200

    added some comments to-- modules/movies.j--s combinging reducers)

--- src/modules/movies.js
+++ src/modules/movies.js
@@ -1,4 +1,7 @@
 const { combineReducers } = require('redux')
+
 const { reducer: movies } = require('./movies')
+//creates a reducer object called movies from the reducer property of movies.js
 
 module.exports = combineReducers({ movies })
+// more recucers can go there.
